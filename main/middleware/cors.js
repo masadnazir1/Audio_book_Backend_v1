f8ef0318d0a1e08af4db170806703d5c7af6d2d5
M Asad Nazir <masadnazir1@gmail.com>
2025-02-09 14:15:21 +0500

    added the cors urls

--- main/middleware/cors.js
+++ main/middleware/cors.js
@@ -1,7 +1,10 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "http://localhost:5173", // Allow frontend origin
+  origin: [
+    "http://localhost:5173", // Allow localhost for development
+    "https://storybook.galaxydev.pk", // Allow your storybook domain
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
   credentials: true, // Allow cookies if needed
 };
